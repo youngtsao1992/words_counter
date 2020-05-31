1184c38591276faae0effefab474e9d45b214d1c
yangcao <yang.a.cao@intel.com>
2020-05-31 20:40:03 +0800

    modify start to add run type choose
    Signed-off-by: yangcao <yang.a.cao@intel.com>

--- start.bat
+++ start.bat
@@ -2,6 +2,11 @@
 
 cd %~dp0
 
+echo Select run type: 0-SHELL 1-GUI
+set /p run= 
+
+if %run% EQU 1 goto :UI
+
 echo Hello Miss Tang,
 echo       Welcome to TANGTANG WORD COUNTER
 echo.
@@ -12,5 +17,9 @@ echo 2/2 Please enter the words you want to counter:
 set /p input_words=
 echo Counting...
 call python word_counter.py "%input_file%" -w "%input_words%"
-
 pause
+
+:UI
+call python word_counter_ui.py
+
+:EOF
